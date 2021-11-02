1dc8997079cd115f22cc4a27f1365b6f119aec8a
git <jeff@bigboy.us>
2021-11-02 09:38:12 -0400

    Initial Commit

---
+++ Shared/jeffs_webviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  jeffs_webviewApp.swift
+//  Shared
+//
+//  Created by Jeff Sisson on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct jeffs_webviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
